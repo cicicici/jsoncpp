60c18d282fe4689c7107715b6e5be0294221026b
cicicici <cicicici@gmail.com>
2018-01-01 16:17:10 -0800

    Override JSONCPP_DEPRECATED, disable warning message

--- json.h
+++ json.h
@@ -249,6 +249,8 @@ license you like.
 #  endif  // GNUC version
 #endif // __clang__ || __GNUC__
 
+#undef JSONCPP_DEPRECATED
+
 #if !defined(JSONCPP_DEPRECATED)
 #define JSONCPP_DEPRECATED(message)
 #endif // if !defined(JSONCPP_DEPRECATED)
